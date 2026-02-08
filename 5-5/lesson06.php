5ceb4071dbbbe83c2de5bc409f9fd9b2386101b7
namp7119 <namp.sw.7119@gmail.com>
2026-02-08 16:05:15 +0900

    lesson06完了

--- 5-5/lesson06.php
+++ 5-5/lesson06.php
@@ -1,1 +1,24 @@
 ﻿<?php
+
+$start_num = 5;
+$rows = 5;
+
+for ($i = 0; $i < $rows; $i++) {
+    for ($s = 1; $s <= ($rows - $i - 1); $s++) {
+        echo " ";
+    }
+
+    $current_min = $start_num - $i;
+
+    for ($j = $start_num; $j >= $current_min; $j--){
+        echo $j;
+    }
+
+    for ($k = $current_min + 1; $k <= $start_num; $k++) {
+        echo $k;
+    }
+
+    echo "\n";
+}
+
+
