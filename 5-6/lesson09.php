56d5b43fc9308e36521548bb19d74d1617ed544a
namp7119 <namp.sw.7119@gmail.com>
2026-02-08 20:30:21 +0900

    lesson09完了

--- 5-6/lesson09.php
+++ 5-6/lesson09.php
@@ -1,2 +1,10 @@
 ﻿<?php
+
+for ($year = 1980; $year <= 2080; $year++) {
+    if (($year % 4 === 0 && $year % 100 !== 0) || ($year % 400 ===0)) {
+        echo "{$year}年はうるう年です。 \n";
+    } else {
+        echo "{$year}年 \n";
+    }
+}
 ?>
