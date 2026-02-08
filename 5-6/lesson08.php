a510d6badd6b006ad2a7ac1162ce4e3a43301735
namp7119 <namp.sw.7119@gmail.com>
2026-02-08 17:29:05 +0900

    lesson08完了

--- 5-6/lesson08.php
+++ 5-6/lesson08.php
@@ -1,2 +1,16 @@
 ﻿<?php
+$arr = [99, 3, 12, 45, 60, 100, 31, 7, 28];
+$n = count($arr);
+
+for ($i = 0; $i < $n - 1; $i++) {
+    for ($j = 0; $j < $n - 1 - $i; $j++) {
+        if ($arr[$j] > $arr[$j + 1]) {
+            $temp = $arr[$j];
+            $arr[$j] = $arr[$j + 1];
+            $arr[$j + 1] = $temp;
+        }
+    }
+}
+
+echo implode(', ', $arr);
 ?>
