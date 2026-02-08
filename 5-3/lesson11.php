f72d775b2202dbd6ceeafbc0a895461dca220355
namp7119 <namp.sw.7119@gmail.com>
2026-02-08 14:04:15 +0900

    lesson11再々提出

--- 5-3/lesson11.php
+++ 5-3/lesson11.php
@@ -1,28 +1,16 @@
 <?php
 
-$users = [
-    [
-        "name" => "田中",
-        "age" => "25",
-        "gender" => "男"
-    ],
-    [
-        "name" => "鈴木",
-        "age" => "20",
-        "gender" => "男"
-    ],
-    [  
-        "name" => "佐藤",
-        "age" => "23",
-        "gender" => "女"
-    ]
+$data = [
+    ['name'=>'田中', 'age'=>'25', 'gender'=>'男'],
+    ['name'=>'鈴木', 'age'=>'20', 'gender'=>'男'],
+    ['name'=>'佐藤', 'age'=>'23', 'gender'=>'女'],
 ];
 
-foreach ($users as $user) {
+foreach ($data as $user) {
     echo $user["name"] . $user["age"] . $user["gender"] . "\n";
 }
 
-foreach ($users as $user) {
+foreach ($data as $user) {
     if ($user["name"] === "鈴木") {
         echo $user["age"] ;
     }
