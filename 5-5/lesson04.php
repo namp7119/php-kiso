7475c8e3c6fd103587db62ee8adc50fb06c07971
namp7119 <namp.sw.7119@gmail.com>
2026-02-08 15:17:16 +0900

    lesson04完了

--- 5-5/lesson04.php
+++ 5-5/lesson04.php
@@ -1,1 +1,21 @@
 ﻿<?php
+
+$max = 4;
+
+for ($i = 1; $i <= $max; $i++) {
+    for ($s = 1; $s <= ($max - $i); $s++) {
+        echo " ";
+    }
+
+    for ($j = 1; $j <= $i; $j++){
+        echo $j;
+    }
+
+    for ($k = $i - 1; $k >= 1; $k--) {
+        echo $k;
+    }
+
+    echo "\n";
+}
+
+
