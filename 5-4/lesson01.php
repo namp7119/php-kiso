c86caf7f06bebeb62e6afd1783d360b5cfe91085
namp7119 <namp.sw.7119@gmail.com>
2026-02-09 20:41:23 +0900

    lesson01完了

--- 5-4/lesson01.php
+++ 5-4/lesson01.php
@@ -1,11 +1,7 @@
 ﻿<?php
-<<<<<<< HEAD
 for ($i = 1; $i <= 3; $i++) {
     for ($j = $i; $j >= 1; $j--) {
       echo $j;  
     }
     echo "\n";
 }
-=======
-unregister_tick_function
->>>>>>> d44a8131ff37623e8367bf03de5284a5ccaca301
