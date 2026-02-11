210c5404599536615ab6cae582c1caad332590c2
namp7119 <namp.sw.7119@gmail.com>
2026-02-11 20:59:06 +0900

    lesson05完了

--- 5-8/lesson05.php
+++ 5-8/lesson05.php
@@ -1,1 +1,25 @@
 <?php
+
+$arr = [5, 2, 7, 9, 8, 1, 3, 6, 4];
+$n = count($arr);
+$count = 0;
+
+
+
+for ($i = 0; $i < $n - 1; $i++) {
+    for ($j = 0; $j < $n - 1 - $i; $j++) {
+        if ($arr[$j] > $arr[$j + 1]) {
+            $temp = $arr[$j];
+            $arr[$j] = $arr[$j + 1];
+            $arr[$j + 1] = $temp;
+
+            $count++;
+            echo "入れ替え{$count}回目\n";
+            print_r($arr);
+        }
+    }
+}
+
+echo "\n結果\n";
+print_r($arr);
+?>
