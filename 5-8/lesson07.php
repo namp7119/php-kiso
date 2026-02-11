1ebe9e6418319031a4d2159cfab17cc5b4758740
namp7119 <namp.sw.7119@gmail.com>
2026-02-11 23:18:23 +0900

    lesson07完了

--- 5-8/lesson07.php
+++ 5-8/lesson07.php
@@ -1,1 +1,24 @@
 ﻿<?php
+
+$numbers = [2, 9, 7, 5, 8, 1, 3, 4, 6];
+
+$target = 1;
+
+$found = false;
+$count = 0;
+
+foreach ($numbers as $index => $value) {
+    $count++;
+
+    if ($value === $target) {
+        echo "探索回数{$count}回\n";
+        echo "要素はインデックス{$index}にあります。\n";
+        $found = true;
+        break;
+    }
+}
+    
+if (!$found) {
+    echo "探索回数{$count}回\n";
+    echo "値{$target}は見つかりませんでした。";
+}
