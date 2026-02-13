b5d773454f1ccbb26143f4350fd8e8a1ea2c2f7b
namp7119 <namp.sw.7119@gmail.com>
2026-02-13 18:40:31 +0900

    lesson10完了

--- 5-9/lesson10.php
+++ 5-9/lesson10.php
@@ -1,1 +1,30 @@
 ﻿<?php
+
+$towers = [
+    'A' => [3, 2, 1],
+    'B' => [],
+    'C' => []
+];
+
+echo "⬛︎実行前\n";
+print_r($towers);
+
+function hanoi_sim($n, $source_key, $target_key, $helper_key, &$towers) {
+    if ($n === 1) {
+        $disk = array_pop($towers[$source_key]);
+        array_push($towers[$target_key], $disk);
+        echo "{$source_key} から {$target_key} に移動\n";
+        return;
+    }
+
+    hanoi_sim($n - 1, $source_key, $helper_key, $target_key, $towers);
+
+    hanoi_sim(1, $source_key, $target_key, $helper_key, $towers);
+
+    hanoi_sim($n - 1, $helper_key, $target_key, $source_key, $towers);
+}
+
+hanoi_sim(3, 'A', 'C', 'B', $towers);
+
+echo "⬛︎実行後\n";
+print_r($towers);
