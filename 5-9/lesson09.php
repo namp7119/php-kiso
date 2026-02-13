f9059e1ed6eabe62e619e6a0daf024e05c101b07
namp7119 <namp.sw.7119@gmail.com>
2026-02-13 17:47:00 +0900

    lesson09完了

--- 5-9/lesson09.php
+++ 5-9/lesson09.php
@@ -1,1 +1,28 @@
 ﻿<?php
+
+function findGCD($a, $b) {
+
+    while ($b !== 0) {
+        $diff = abs($a - $b);
+
+        echo $diff . "\n";
+
+        if ($a > $b) {
+            $a = $a % $b;
+        } else {
+            $b = $b % $a;
+        } 
+
+        if ($a === 0) return $b;
+        if ($b === 0) return $a;
+    }
+}
+
+$num1 = 1116;
+$num2 = 708;
+
+$gcd = findGCD($num1, $num2);
+
+echo "{$num1}と{$num2}の最大公約数は{$gcd}\n";
+
+
