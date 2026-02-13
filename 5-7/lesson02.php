02e8498f92e8309ecbf806931374c61a5738f440
namp7119 <namp.sw.7119@gmail.com>
2026-02-14 02:15:12 +0900

    lesson02完了

--- 5-7/lesson02.php
+++ 5-7/lesson02.php
@@ -4,46 +4,39 @@ $stack = [];
 
 while (true) {
     echo "\n⬜︎モード選択 (1:Push, 2:Pop, 3:Peek, 4:IsEmpty, その他:終了): ";
+   
     $mode = trim(fgets(STDIN));
 
-    if ($mode !== '1' && $mode !== '2' && $mode !== '3' && $mode !== '4') {
-        echo "プログラムを終了します。 \n";
-        break;
-    }
-
-    switch ($mode) {
-        case '1':
+    if ($mode == '1') {
             echo "「データ入力」をしてください: ";
             $data =trim(fgets(STDIN));
             array_push($stack, $data);
             echo "結果: {$data}をPushしました。 \n";
-            break;
 
-        case '2':
+        } elseif ($mode == '2') {
             if (empty($stack)) {
                 echo "結果: empty (取り出すデータがありません）\n";
             } else {
                 $popped = array_pop($stack);
                 echo "結果: {$popped} をPopしました。 \n";
             }
-            break;
 
-        case '3':
+        } elseif ($mode == '3') {
             if (empty($stack)) {
                 echo "結果: empty (スタックは空です)\n";
             } else {
                 $top = end($stack);
                 echo "結果: 現在の一番上(Peek)は{$top} です。\n";
             }   
-            break;
 
-        case '4':
+        } elseif ($mode == '4') {
             if (empty($stack)) {
                 echo "empty\n";
             } else {
                 echo "not empty \n";
             }
-            break;
+    } else {
+        echo "プログラムを終了します。 \n";
+        break;
     }
-
 }
