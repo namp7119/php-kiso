e4f2f370e41409edb249e6c4f4997653cb76d3c6
namp7119 <namp.sw.7119@gmail.com>
2026-02-17 22:15:28 +0900

    lesson02完了

--- 5-11/lesson02.php
+++ 5-11/lesson02.php
@@ -12,27 +12,21 @@ class Account {
     }
 
     public function deposit($amount) {
-        if ($amount > 0) {
-            $this->balance += $amount;
-        }
-    }    
+        if ($amount > 0) $this->balance += $amount;
+        }  
 
     public function withdraw($amount) {
         if ($amount > $this->balance) {
             echo "残高不足です。\n";
-
         } elseif($amount > 0) {
             $this->balance -= $amount;
         }
     }
 
-    public function getBalance() {
-        return $this->balance;
-    }
 
-    public function getDetails() {
-        return "口座番号: {$this->accountNumber} | 種類: {$this->type} | 残高:  ¥" . $this->balance;
-    }
+    public function getBalance() {return $this->balance;}
+    public function getAccountNumber() {return $this->accountNumber;}
+    public function getType() {return $this->type;}
 }
 
 
@@ -46,46 +40,49 @@ class Customer  {
         $this->account = $account;
     }
 
-    public function getName() {
-        return $this->name;
-    }
+    public function getName() {return $this->name;}
+    public function getAccount() {return $this->account;}
 
     public function getAccountDetails() {
-        return "顧客名: {$this->name} | " . $this->account->getDetails();
-    }
-
-    public function getAccount() {
         return $this->account;
-    }
-}    
+    }    
+}
+
 
 class Bank {
     private $customers =[];
 
-   public function addCustomer($customer) {
+   public function addCustomer(Customer $customer) {
         $this->customers[] = $customer;
    }
 
    public function getCustomerInfo($name) {
         foreach ($this->customers as $customer) {
             if ($customer->getName() === $name) {
-                return $customer-> getAccountDetails() . "\n";
+                return $customer;
             }
-        }
-        return "顧客名: {$name}が見つかりませんでした。\n";
+        }    
+
+        return null;
         
    } 
 }   
 
 
 $account1 = new Account("123456" , "普通預金");
-
 $customer1 =new Customer("澤口" , $account1);
+$bank = new Bank();
+
+$bank->addCustomer($customer1);
 
 $account1->deposit(10000);
 $account1->withdraw(3000);
 
-$bank = new Bank();
-$bank->addCustomer($customer1);
-
-echo $bank->getCustomerInfo("澤口");
+$info = $bank->getCustomerInfo("澤口");
+if ($info) {
+    $a = $info->getAccount();
+    echo "銀行口座情報" . PHP_EOL;
+    echo "顧客: " . $info->getName() . PHP_EOL;
+    echo "口座: " . $a->getAccountNumber() . " [" . $a->getType() . "]" . PHP_EOL;
+    echo "残高: " . number_format($a->getBalance()) . "円" . PHP_EOL;
+}
