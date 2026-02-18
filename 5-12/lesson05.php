9848514d675d882059a16d563ab8dbbe5a59558b
namp7119 <namp.sw.7119@gmail.com>
2026-02-18 20:14:17 +0900

    lesson05完了

--- 5-12/lesson05.php
+++ 5-12/lesson05.php
@@ -1,1 +1,52 @@
 ﻿<?php
+
+abstract class Media {
+    abstract public function play();
+}
+
+class Music extends Media {
+    private $songTitle;
+
+    public function __construct($songTitle) {
+        $this->songTitle = $songTitle;
+    }
+
+    public function play() {
+        return "「{$this->songTitle}を再生中」";
+    }
+
+}
+
+class Video extends Media {
+    private $videoTitle;
+
+    public function __construct($videoTitle) {
+        $this->videoTitle = $videoTitle;
+    }
+
+    public function play() {
+        return "「{$this-> videoTitle}を再生中」";
+    }
+}
+
+class Podcast extends Media {
+    private $episodeTitle;
+
+    public function __construct($episodeTitle) {
+        $this->episodeTitle = $episodeTitle;
+    }
+
+    public function play() {
+        return "「{$this-> episodeTitle}を再生中」";
+    }
+}
+
+$playlist = [
+    new Music("[ディズニー]"),
+    new Video("[旅行Vlog]"),
+    new Podcast("[英会話]"),
+];
+
+foreach ($playlist as $item) {
+    echo $item->play() . PHP_EOL;
+}
